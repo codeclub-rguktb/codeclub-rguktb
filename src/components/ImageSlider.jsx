098d771be3e9ff4631fc89474557b6bb78df1f0d
Udaykiran <officialudaykiran@gmail.com>
2022-09-15 17:35:14 +0530

    gallery

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -3,8 +3,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-import Image from "../Images/ss.png";
 import Image1 from "../Images/slider/image1.jpeg";
+import Image2 from "../Images/slider/image2.jpg";
+import Image3 from "../Images/slider/image3.jpg";
+import Image4 from "../Images/slider/image4.jpg";
 
 export default function SimpleSlider() {
 
@@ -16,13 +18,18 @@ export default function SimpleSlider() {
         },
         {
             id: 2,
-            src: Image1,
-            alt: 'Image 1'
+            src: Image2,
+            alt: 'Image 2'
         },
         {
             id: 3,
-            src: Image1,
-            alt: 'Image 1'
+            src: Image3,
+            alt: 'Image 3'
+        },
+        {
+            id: 3,
+            src: Image4,
+            alt: 'Image 4'
         },
     ]
     const settings = {
@@ -45,7 +52,7 @@ export default function SimpleSlider() {
                                 <div class="carousel-item active relative float-left w-full rounded-lg">
                                     <img
                                         src={item.src}
-                                        class="block w-full rounded-lg" style={{ height: '500px' }}
+                                        class="block w-full rounded-lg" style={{ height: '550px' }}
                                         alt={item.alt}
 
                                     />
